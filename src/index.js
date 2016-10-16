f88838c02a1968dd2339a2e650c6a84d6c9dd339
Marcin Szczepanczyk <marcin.szczepanczyk@unic.com>
2016-10-16 10:16:52 +0200

    gh-pages

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'babel-polyfill';
-import 'zone.js/dist/zone';
-import 'raw-loader/index';
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
